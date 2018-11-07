40d9cd77a3e3a3632894ee280e0379bc0e06f64d
Connie Hyman <constance.hyman@mchsi.com>
2018-11-07 23:41:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,10 +14,10 @@ class Application
       elsif req.path.match(/search/)
         search_term = req.params["q"]
         resp.write handle_search(search_term)
-      elsif req.path.match(/add/)
-        item = req.params["q"]
-        # resp.write "We don't have that item"
-        resp.write handle_add(item)
+      # elsif req.path.match(/add/)
+      #   item = req.params["q"]
+      #   # resp.write "We don't have that item"
+      #   resp.write handle_add(item)
       elsif req.path.match(/cart/)
         if @@cart.empty?
           resp.write "Your cart is empty"
